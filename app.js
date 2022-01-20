73dfea3542e4e24c3a90446fc7636c2876cb4c5c
Theodore Stansbury <tstansbu.27@gmail.com>
2022-01-20 15:50:38 -0500

    Added style using Bulma

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const button1 = document.querySelector('#p1Button');
-const button2 = document.querySelector('#p2Button');
+const p1Button = document.querySelector('#p1Button');
+const p2Button = document.querySelector('#p2Button');
 const resetButton = document.querySelector('#reset')
 const p1Display = document.querySelector('#p1Score');
 const p2Display = document.querySelector('#p2Score');
@@ -10,37 +10,49 @@ let p2Points = 0;
 let winningScore = 3;
 let isGameOver = false;
 
-button1.addEventListener('click', () => {
+p1Button.addEventListener('click', () => {
     if (!isGameOver) {
         p1Points += 1;
         if (p1Points === winningScore) {
             isGameOver = true;
+            p1Display.classList.add('has-text-success');
+            p2Display.classList.add('has-text-danger');
+            p1Button.disabled = true;
+            p2Button.disabled = true;
         }
         p1Display.textContent = p1Points;
     }
 })
 
-button2.addEventListener('click', () => {
+p2Button.addEventListener('click', () => {
     if (!isGameOver) {
         p2Points += 1;
         if (p2Points === winningScore) {
             isGameOver = true;
+            p1Display.classList.add('has-text-danger');
+            p2Display.classList.add('has-text-success');
+            p1Button.disabled = true;
+            p2Button.disabled = true;
         }
         p2Display.textContent = p2Points;
     }
 })
 
-const reset = () => {
-    isGameOver = false;
-    p1Points = 0;
-    p1Display.textContent = 0;
-    p2Points = 0;
-    p2Display.textContent = 0;
-}
+resetButton.addEventListener('click', reset);
 
 winningScoreSelect.addEventListener('change', function () {
     winningScore = parseInt(this.value);
     reset();
 })
 
-resetButton.addEventListener('click', reset);
+function reset() {
+    isGameOver = false;
+    p1Points = 0;
+    p1Display.textContent = 0;
+    p1Display.classList.remove('has-text-success', 'has-text-danger');
+    p2Points = 0;
+    p2Display.textContent = 0;
+    p2Display.classList.remove('has-text-success', 'has-text-danger');
+    p1Button.disabled = false;
+    p2Button.disabled = false;
+}
